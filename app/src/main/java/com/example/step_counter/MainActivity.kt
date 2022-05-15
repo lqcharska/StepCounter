66be3fd1ab356b6f5c0069fbe313db3e73fa7158
lullusiek <lullusiek@gmail.com>
2022-05-15 17:49:04 +0200

    begin the project

---
+++ app/src/main/java/com/example/step_counter/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.step_counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.step_counter.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
---
+++ app/src/main/java/com/example/step_counter/ui/main/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.example.step_counter.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.step_counter.R
+
+class MainFragment : Fragment() {
+
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment,
+            container,
+            false)
+    }
+
+
+}
