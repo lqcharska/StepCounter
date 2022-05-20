39e6d1623484d246f3bd003cebcd0205c7e6e8f5
lullusiek <lullusiek@gmail.com>
2022-05-20 20:52:48 +0200

    change folder and add fixes

--- android_app/app/src/main/java/com/example/step_counter/ui/main/MainFragment.kt
+++ android_app/app/src/main/java/com/example/step_counter/ui/main/MainFragment.kt
@@ -17,6 +17,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.step_counter.R
 import com.example.step_counter.databinding.MainFragmentBinding
+import com.google.gson.JsonElement
 import kotlinx.android.synthetic.main.main_fragment.*
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -26,6 +27,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
 import java.io.File
 import java.util.*
 
@@ -41,6 +43,7 @@ class MainFragment : Fragment(), SensorEventListener {
     private lateinit var yAccVector: Vector<Float>
     private lateinit var zAccVector: Vector<Float>
     private lateinit var timeVector: Vector<Long>
+    private lateinit var djangoServer: String
 
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -59,6 +62,11 @@ class MainFragment : Fragment(), SensorEventListener {
 
         sensorManager = activity?.getSystemService(SENSOR_SERVICE) as SensorManager
         binding.accNumber.text = 0.toString()
+
+        djangoServer = if(binding.ipTextInput.text.toString() != "IP address..."){
+            binding.ipTextInput.text.toString()
+        } else "192.168.1.25"
+
 
         binding.startButton.setOnClickListener {
             xAccVector.clear()
@@ -81,23 +89,20 @@ class MainFragment : Fragment(), SensorEventListener {
             val file =
                 File(context?.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "acc_data.csv")
 
-            Toast.makeText(
-                activity,
-                xAccVector.size.toString() + " " + yAccVector.size.toString() + " " + zAccVector.size.toString(),
-                Toast.LENGTH_LONG
-            ).show()
-
             binding.timer.stop()
             pauseOffset = 0
             previousTotalSteps = totalSteps
             binding.accNumber.text = 0.toString()
             sensorManager!!.unregisterListener(this)
 
+            var djangoSite = "http://$djangoServer:2137/"
+
             file.printWriter().use { out ->
+                out.println("x;y;z;time")
                 for (i in 0 until xAccVector.size) {
                     out.println(
                         String.format(
-                            "%.2f, %.2f, %.2f, %d",
+                            "%.2f; %.2f; %.2f; %d",
                             xAccVector[i],
                             yAccVector[i],
                             zAccVector[i],
@@ -106,22 +111,7 @@ class MainFragment : Fragment(), SensorEventListener {
                     )
                 }
             }
-            uploadFile(file)
-//            val TEXT_CSV_TYPE = "text/csv".toMediaTypeOrNull()
-//            val client = OkHttpClient()
-//            val requestBody: RequestBody = file.asRequestBody(TEXT_CSV_TYPE)
-//            val multiPartBody: MultipartBody.Part =
-//                createFormData("acc_data", file.name, requestBody)
-//            val request: Request = Request.Builder()
-//                .url("https://192.168.1.25:2137/get_acc_data/")
-//                .post(requestBody)
-//                .build();
-//            val response: Response = client.newCall(request).execute()
-//            if (!response.isSuccessful) {
-//                Toast.makeText(activity, "erroooooor", Toast.LENGTH_SHORT).show()
-//            } else {
-//                Toast.makeText(activity, "file send", Toast.LENGTH_SHORT).show()
-//            }
+            uploadFile(file, djangoSite)
         }
 
         return binding.root
@@ -146,11 +136,10 @@ class MainFragment : Fragment(), SensorEventListener {
         return
     }
 
-    private fun uploadFile(file: File) {
+    private fun uploadFile(file: File, djangoSite: String) {
 
-        val DjangoSite: String = "http://192.168.1.25:2137/"
         val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(DjangoSite)
+            .baseUrl(djangoSite)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
@@ -158,16 +147,18 @@ class MainFragment : Fragment(), SensorEventListener {
 
         val requestBody: RequestBody = file.asRequestBody("text/csv".toMediaTypeOrNull())
         val multiPartBody: MultipartBody.Part =
-            createFormData("acc_data", file.getName(), requestBody)
+            createFormData("acc_data", file.name, requestBody)
         val call: Call<RequestBody> = postApi.upload(multiPartBody)
 
         call.enqueue(object : Callback<RequestBody> {
             override fun onResponse(call: Call<RequestBody?>?, response: retrofit2.Response<RequestBody?>?) {
+                Toast.makeText(activity, "Success", Toast.LENGTH_LONG).show()
                 Log.d("good", "good")
             }
 
             override fun onFailure(call: Call<RequestBody?>?, t: Throwable?) {
-                Log.d("fail", "fail")
+                Toast.makeText(activity, "Failed: " + t.toString(), Toast.LENGTH_LONG).show()
+                Log.d("fail", t.toString())
             }
         })
 
