d801712cf58f965c5c94f14d47ff3249edd33a66
lullusiek <lullusiek@gmail.com>
2022-05-22 20:57:11 +0200

    change files names

--- django/get_acc_data/views.py
+++ django/get_acc_data/views.py
@@ -7,6 +7,9 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 from django import forms
+from datetime import datetime
+import re
+
 
 
 @csrf_exempt
@@ -16,7 +19,9 @@ def index(request):
         if request.FILES['acc_data']:
             acc_data_file = request.FILES['acc_data']
             fs = FileSystemStorage(location='acc_data_dir')
-            filename = fs.save(acc_data_file.name, acc_data_file)
+            file_name = re.findall(".+?(?=\.)", str(datetime.now()))[0]
+            file_name = file_name.replace(':', '_') + ".csv"
+            filename = fs.save(file_name, acc_data_file)
             response_data['filename'] = filename
     response_data['status'] = 'ok'
     return HttpResponse(status=204)
